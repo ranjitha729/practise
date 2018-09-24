f1bd8c2beb7447a6a0e201eb229f324fc9227137
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2018-09-24 19:03:53 +0530

    Create  Prime.java

---
+++ Prime.java
@@ -0,0 +1,20 @@
+public class Prime {
+      public static void main(String args[]) {
+    	  int i,n,count=0;
+    	 Scanner sc = new Scanner (System.in);
+    	  System.out.println("enter the number");
+    	  n = sc.nextInt();
+    	  for(i=2; i<n;i++) {
+    		 if(n%i==0) {
+    			 count++;
+    		 break;
+    		  }
+    	  }
+    		  if (count==0) {
+    			  System.out.println("yes");
+    		  }
+    		  else {
+    			  System.out.println("no");
+    		  }
+    	  }
+      }

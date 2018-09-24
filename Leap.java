461e6723c39534f175e61139ea1adbe47f23f5d1
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2018-09-24 17:49:38 +0530

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,31 @@
+public class Leap {
+	public static void main(String args[]) {
+	int year;
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter any Year:");
+	year = scan.nextInt();
+	scan.close();
+    boolean isLeap = false;
+
+    if(year % 4 == 0)
+    {
+        if( year % 100 == 0)
+        {
+            if ( year % 400 == 0)
+                isLeap = true;
+            else
+                isLeap = false;
+        }
+        else
+            isLeap = true;
+    }
+    else {
+        isLeap = false;
+    }
+
+    if(isLeap==true)
+        System.out.println("yes");
+    else
+        System.out.println("no");
+}
+}

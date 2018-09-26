be6ec1d43af9e5354729f5603367216c86a956fd
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2018-09-26 18:42:09 +0530

    Create  Odd1.java

---
+++ Odd1.java
@@ -0,0 +1,15 @@
+public class Odd1 {
+	public static void main(String args[]) {
+		int n1;
+		int n2;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter the limits:");
+		n1 = scan.nextInt();
+		n2=scan.nextInt();
+		for (int i = n1; i <= n2; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}

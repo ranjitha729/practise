61230a5089b3d474cd1de9ee1ef796d9eb06017d
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-10 14:02:48 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,16 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class hello {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int f=1;
+		for(int i=1;i<=n;i++){
+			 f = f *i;
+		}
+		System.out.println(f);
+		
+}
+}

c7948d097de0e28b87d9fdac17593c3ff405b4f5
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-11 17:56:13 +0530

    Create countlength.java

---
+++ countlength.java
@@ -0,0 +1,17 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class countstring {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String s = str.replaceAll("\\s", ""); 
+		System.out.println(s.length());
+		
+		
+		
+		
+	
+	}
+}

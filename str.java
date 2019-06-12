62075ac7951d6611e1e245785c5f01112cb25aad
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-12 17:36:55 +0530

    Create str.java

---
+++ str.java
@@ -0,0 +1,14 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class str {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String s = sc.next();
+		for(int i=0;i<n;i++){
+			System.out.println(s);
+		}
+}
+}

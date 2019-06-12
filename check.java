af78de3445c949dd1be21f8e3353ca17834d169a
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-12 17:57:34 +0530

    Create check.java

---
+++ check.java
@@ -0,0 +1,16 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class check {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n>=1 && n<=10){
+			System.out.println("yes");
+		}
+		else{
+			System.out.println("no");
+		}
+}
+}

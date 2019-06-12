c65681c2cf3c4beb16062dc21b98e811a7b85f05
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-12 15:05:46 +0530

    Create multiple5.java

---
+++ multiple5.java
@@ -0,0 +1,13 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class multiple5 {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i =1;i<=5;i++){
+		System.out.println(i*n);
+			}
+		}
+}

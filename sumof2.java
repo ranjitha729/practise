b1b9d1010124360e85123aa373520db2ccafa798
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-13 09:57:48 +0530

    Create sumof2.java

---
+++ sumof2.java
@@ -0,0 +1,13 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class sumof2 {
+	public static void main(String[] args) {
+		int n1,n2,n3=0;
+	Scanner	sc = new Scanner(System.in);		
+	n1 = sc.nextInt();
+	n2 = sc.nextInt();
+	System.out.println(n3 = n1 + n2);
+}
+}

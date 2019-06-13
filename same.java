9ea0dfebbcbf8fa311bdad1143924067d132690c
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-13 14:24:45 +0530

    Create same.java

---
+++ same.java
@@ -0,0 +1,12 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class same {
+	public static void main(String[] args) {
+		Scanner	sc = new Scanner(System.in);		
+	String s = sc.next();
+	int k = sc.nextInt();
+	System.out.println(s.substring(0,k));
+  }
+}

f4e4c7a78955a8dff2c22f58c247524eac741046
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-13 14:44:26 +0530

    Rename lessno.java to smallno.java

---
+++ smallno.java
@@ -0,0 +1,12 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class nextno {
+	public static void main(String[] args) {
+		int n, count=0;
+	Scanner	sc = new Scanner(System.in);		
+	n = sc.nextInt();
+	System.out.println(n-1);
+}
+}

106f5b4109f33004b02e791603e9d1523b8349fc
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-13 14:38:43 +0530

    Create multiple7.java

---
+++ multiple7.java
@@ -0,0 +1,19 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class sumof2 {
+	public static void main(String[] args) {
+		int n;
+		Scanner sc = new Scanner (System.in);
+   	  System.out.println("enter the number");
+   	  n = sc.nextInt();
+   	  if(n % 7==0){
+   		  
+   		  System.out.println("yes");
+   	  }
+   	  else{
+   		  System.out.println("no");
+   	  }
+   	  }
+	}

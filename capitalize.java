9e7b83a41246b03123d744ec7c475b7ef96862cf
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-13 15:08:44 +0530

    Update capitalize.java

--- capitalize.java
+++ capitalize.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class camelcase {
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
-		String str=sc.nextLine();
-		for(String i : str.split(" ")){
+		String s=sc.nextLine();
+		for(String i : s.split(" ")){
 		String r = i.substring(0, 1);
 		System.out.print(r.toUpperCase() + i.substring(1).toLowerCase()+" ");
 		}

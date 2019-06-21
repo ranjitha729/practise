9cc7ab480791319910411981216eeb5161e59963
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-21 15:11:11 +0530

    Create ascii.java

---
+++ ascii.java
@@ -0,0 +1,12 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class concat {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		int ascii = c;
+		System.out.println(ascii);
+}
+} 

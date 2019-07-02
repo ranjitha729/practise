4754a31d4958f3f070351b5711cf3b47547ac753
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-07-02 11:53:17 +0530

    Update repeat.java

--- repeat.java
+++ repeat.java
@@ -1,16 +1,33 @@
+package guvi;
+
 import java.util.Arrays;
 import java.util.Scanner;
-public class fghh {
-public static void main(String[] args) {
-Scanner scan=new Scanner(System.in);
-int n=scan.nextInt();
-int a[]=new int[n];
-for(int i=0;i<n;i++){
-a[i]=scan.nextInt();
-}
-Arrays.sort(a);
-for(int i=n-1;i>=0;i--){
-System.out.print(a[i]);
-}
-}
-}
+
+public class str {
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		int val=0;
+		for(int i=0;i<n;i++){
+			a[i] = sc.nextInt();
+		}
+		for(int i=0;i<n;i++){
+			for(int j=i+1;j<n;j++){
+				if(a[i]==a[j]){
+					val = a[i];
+					System.out.println(val);
+                    break;
+				}
+				
+				}
+			}
+		if(val ==0){
+			System.out.println("unique");
+		}
+		}
+		
+		
+		}
+		
+	
